8acbc02f7a21b90e6e8e4dd9c00b4f0d4aa8defa
Ayan <627311610@qq.com>
2022-03-10 00:21:09 +0800

    feat: eslint-config

--- scripts/afterBuild.js
+++ scripts/afterBuild.js
@@ -1,12 +1,10 @@
-const fe = require('fs-extra');
+const fse = require('fs-extra');
 const { resolve } = require('path');
 
 const root = resolve(process.cwd(), 'build/app.content');
 const htmlDir = resolve(root, 'src/app/pages');
-const htmlFiles = fe.readdirSync(htmlDir);
+const htmlFiles = fse.readdirSync(htmlDir);
 if (htmlFiles && htmlFiles.length) {
-  for (const file of htmlFiles) {
-    fe.copyFileSync(resolve(htmlDir, file), resolve(root, file));
-  }
+  htmlFiles.forEach((file) => fse.copyFileSync(resolve(htmlDir, file), resolve(root, file)));
 }
-fe.rmSync(resolve(root, 'src'), { recursive: true });
+fse.rmSync(resolve(root, 'src'), { recursive: true });
--- scripts/cleanBuildDir.js
+++ scripts/cleanBuildDir.js
@@ -1,4 +1,5 @@
 const path = require('path');
 const fs = require('fs-extra');
+
 const dir = `${path.join(__dirname, '../build/output')}`;
 fs.removeSync(dir);
