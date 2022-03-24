06fcf46d951998be9b1690d5ede503c85c0590a7
张宇雁 <zhangyuyan.1024@bytedance.com>
2022-03-25 00:54:48 +0800

    feat: plugin for mpa entry file; add LICENSE;

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -7,22 +7,10 @@ const RENDERER_BUILD_DIR = resolve(BUILD_DIR, 'renderer');
 const PRELOAD_BUILD_DIR = resolve(BUILD_DIR, 'preload');
 const MAIN_BUILD_DIR = resolve(BUILD_DIR, 'main');
 
-/**
- * move entry files to the renderer root directory.
- * for example: 'build/renderer/pages/home/index.html' -> 'build/renderer/home.html'
- */
-function handleMoveEntry() {
-  const entryDir = resolve(RENDERER_BUILD_DIR, 'pages');
-  const entryNames = fse.readdirSync(entryDir);
-  entryNames.forEach((entryName) => fse.moveSync(resolve(entryDir, entryName, 'index.html'), resolve(RENDERER_BUILD_DIR, `${entryName}.html`)));
-  fse.removeSync(entryDir);
-}
-
 (async () => {
   // build renderer
   fse.emptyDirSync(RENDERER_BUILD_DIR);
   await build({ configFile: 'src/renderer/vite.config.js' });
-  handleMoveEntry();
 
   // build preload
   fse.emptyDirSync(PRELOAD_BUILD_DIR);
--- src/renderer/vite.config.js
+++ src/renderer/vite.config.js
@@ -4,13 +4,19 @@ import { resolve } from 'path';
 // A vite plugin for import ui library component style automatic.
 // https://github.com/onebay/vite-plugin-imp
 import vitePluginImp from 'vite-plugin-imp';
+/**
+ * move entry files to the renderer root directory.
+ * e.g: 'build/renderer/pages/home/index.html' -> 'build/renderer/home.html'
+ * https://github.com/RyanProMax/vite-plugin-transfer-mpa
+ */
+import mpa from '@ryanpromax/vite-plugin-transfer-mpa';
 import pkg from '../../package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: process.env.NODE_ENV,
   root: __dirname,
-  plugins: [react(), vitePluginImp()],
+  plugins: [react(), vitePluginImp(), mpa()],
   base: './',
   build: {
     sourcemap: true,
